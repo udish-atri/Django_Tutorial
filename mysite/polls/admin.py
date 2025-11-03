feea270cc0e7598b25d0fe71f9e40f285c911be4
udish-atri <udishkum@atrihub.io>
2025-11-02 20:17:14 -0800

    feat(django-training-part7): customizing admin page
    customized and optimised the Django admin page
    
    [ICTRAINING-2236]

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -12,6 +12,7 @@ class QuestionAdmin(admin.ModelAdmin):
         (None, {"fields": ["question_text"]}),
         ("Date information", {"fields": ["pub_date"]}),
     ]
+    # fields = ["pub_date", "question_text"]
     inlines = [ChoiceInline]
     list_display = ["question_text", "pub_date", "was_published_recently"]
     list_filter = ["pub_date"]
@@ -19,3 +20,4 @@ class QuestionAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
